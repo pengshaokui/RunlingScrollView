8e4f1f801868eb6ea0d59bb4931bb6be0af0e29d
shaokui <524490971@qq.com>
2018-03-15 14:44:57 +0800

    Initial commit

---
+++ app/src/main/java/com/scrollview/runling/pengsk/runlingscrollview/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.scrollview.runling.pengsk.runlingscrollview
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
